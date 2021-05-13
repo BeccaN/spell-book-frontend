65c8cd4b5e988d28e7e51f4dd9cc9184b2d5f9c3
rncongi <rncongi@gmail.com>
2021-05-13 00:15:47 -0400

    FETCH_SPELLS action works with table!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import SpellInfo from './components/SpellInfo'
 import SpellContainer from './components/SpellContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {connect} from 'react-redux'
 import { fetchSpells } from './actions/fetchSpells'
 
-function App(props) {
-  const [spells, setSpells] = useState(null)
+function App() {
   const [selectedSpell, setSelectedSpell] = useState(null)
-
-  useEffect(() => {
-    const apiUrl = 'http://localhost:8000/api/v1/spells'
-    fetch (apiUrl)
-      .then((resp) => resp.json())
-      .then((data) => setSpells(data))
-  }, [])
-
-  // useEffect(() => {
-  //   props.fetchSpells()
-  // })
 
   const handleSelect = (data) => {
     setSelectedSpell(data)
@@ -31,7 +19,7 @@ function App(props) {
       </header>
       <div className="row">
         {}
-        <SpellContainer spells={spells} handleSelect={handleSelect}/>
+        <SpellContainer handleSelect={handleSelect}/>
         <SpellInfo selectedSpell={selectedSpell} />
       </div>
     </div>
--- src/components/SpellContainer.js
+++ src/components/SpellContainer.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import SpellTable from './table/SpellTable'
+import { fetchSpells } from '../actions/fetchSpells'
 
 class SpellContainer extends Component {
+    componentDidMount() {
+        this.props.fetchSpells() 
+    }
+    
     render() {
         return (
             <div className='col-md p-3 m-2 border bg-light shadow'>
-                {this.props.spells != null ? (
-                    <SpellTable spells={this.props.spells} handleSelect={this.props.handleSelect} />
-                ) : null}
+                {this.props.spells.length === 0 ? (
+                    null                
+                    ) : <SpellTable spells={this.props.spells} handleSelect={this.props.handleSelect} />
+                }            
             </div>
         )
     }
 }
 
-export default SpellContainer
+const mapStateToProps = state => {
+    return {
+        spells: state.spells.spells
+    }
+}
+
+export default connect(mapStateToProps, {fetchSpells})(SpellContainer)
