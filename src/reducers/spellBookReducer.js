dc8f560e5fd6f39f24e904c87f8c8fd66e2f3cdc
rncongi <rncongi@gmail.com>
2021-05-17 16:46:47 -0400

    adding spell to spell book functionality

--- src/reducers/spellBookReducer.js
+++ src/reducers/spellBookReducer.js
@@ -6,6 +6,10 @@ export default function spellBookReducer(state = {spell_books: []}, action) {
     case 'ADD_SPELL_BOOK': {
       return {...state, spell_books: [...state.spell_books, action.payload]}
     }
+    case 'UPDATE_SPELL_BOOK': {
+      let filteredState = state.spell_books.filter(book => book.id !== action.payload.id)
+      return {...state, spell_books: [...filteredState, action.payload]}
+    }
     default:
       return state
   }
---
+++ src/reducers/spellSpellBookReducer.js
@@ -0,0 +1,13 @@
+export default function spellSpellBookReducer(state = {spell_spell_books: []}, action) {
+  switch(action.type) {
+    case 'FETCH_SPELL_SPELL_BOOKS': {
+      return {spell_spell_books: action.payload}
+    }
+    case 'DELETE_SPELL_SPELL_BOOK': {
+      let filteredState = state.spell_spell_books.filter(join => join.id !== action.data.id)
+      return {...state, spell_spell_books: filteredState}
+    }
+    default:
+      return state
+  }
+}
