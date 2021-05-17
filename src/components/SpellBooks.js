6c2926c5ef655883a93a5350536cc6b0dcdef531
rncongi <rncongi@gmail.com>
2021-05-16 21:45:42 -0400

    Add spell form created

--- src/components/SpellBooks.js
+++ src/components/SpellBooks.js
@@ -8,9 +8,13 @@ class SpellBooksContainer extends Component {
 
     return (
       <div>
-        <SpellBookForm />
-        <h3>All Spell Books</h3>
-        {this.props.spell_books.map(book => <li><Link to={`/spellbooks/${book.id}`}>{book.title}</Link></li>)}
+        <div className="">
+          <SpellBookForm />
+        </div>
+        <div>
+          <h3>All Spell Books</h3>
+          {this.props.spell_books.map(book => <li><Link to={`/spellbooks/${book.id}`}>{book.title}</Link></li>)}
+        </div>
       </div>
     )
   }
