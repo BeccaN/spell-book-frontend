f5498344469bfafd6b73edbdb6f4aa7b13667e3e
rncongi <rncongi@gmail.com>
2021-05-17 23:05:58 -0400

    Added sorting to spells in spell book show

--- src/components/SpellBookShow.js
+++ src/components/SpellBookShow.js
@@ -3,15 +3,18 @@ import SpellInfo from './SpellInfo'
 
 export default function SpellBookShow(props) {
   const spell_book = props.spell_books.find(book => book.id === parseInt(props.match.params.id))
-  // const spell_book_spells = props.spell_spell_books.filter(join => join.spell_book_id === spell_book.id)
-
+  
+  const spells = (spell_book) ? spell_book.spells.sort(function(a, b) {
+    return a.level - b.level
+  }) : null
+  
   return (
     <div>
       {spell_book ? <h1>{spell_book.title}</h1> : null}
   
       {spell_book ? 
         <div className="spells">
-          {spell_book.spells.map(spell => <div className="spell" key={spell.id}><SpellInfo spell={spell} spell_book_id={spell_book.id} spell_spell_books={props.spell_spell_books} /></div>)}
+          {spells.map(spell => <div className="spell" key={spell.id}><SpellInfo spell={spell} spell_book_id={spell_book.id} spell_spell_books={props.spell_spell_books} /></div>)}
         </div>
       : null}
     </div>
