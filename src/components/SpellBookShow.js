996eda3e03f6532369b34e46ac8807cbe757ebb6
rncongi <rncongi@gmail.com>
2021-05-18 13:30:37 -0400

    SpellBookShow class component w/ delete redirect

--- src/components/SpellBookShow.js
+++ src/components/SpellBookShow.js
@@ -1,39 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
 import SpellInfo from './SpellInfo'
+import {deleteSpellBook} from '../actions/deleteSpellBook'
+import {useHistory} from 'react-router-dom'
 
-class SpellBookShow extends Component {
-  state = {
-    search: ""
-  }
-
-  handleChange = (e) => {
-    this.setState({
-      search: e.target.value
-    })
-  }
-
-  render() {
-
-    const spell_book = this.props.spell_books.find(book => book.id === parseInt(this.props.match.params.id))
+function SpellBookShowFunc(props) {
+  const spell_book = props.spell_books.find(book => book.id === parseInt(props.match.params.id))
   
-    let spells = (spell_book) ? spell_book.spells.sort(function(a, b) {
-      return a.level - b.level
-    }) : null
+  let spells = (spell_book) ? spell_book.spells.sort(function(a, b) {
+    return a.level - b.level
+  }) : null
 
-    return (
-      <div>
-        <div className="d-flex">
-        {spell_book ? <h1>{spell_book.title}</h1> : null}
-        <input onChange={this.handleChange} value={this.state.search} />
-        </div>
-        {spell_book ? 
-          <div className="spells">
-            {spells.map(spell => <div className="spell" key={spell.id}><SpellInfo spell={spell} spell_book_id={spell_book.id} spell_spell_books={this.props.spell_spell_books} /></div>)}
-          </div>
-        : null}
-      </div>
-    )
+  let history = useHistory()
+  
+  const handleClick = (e) => {
+    props.deleteSpellBook(e.target.value)
+    history.push('/spellbooks')
   }
+
+  return (
+    <div>
+      <div className="d-flex">
+        {spell_book ? <h1>{spell_book.title}</h1> : null}
+      </div>
+      {spell_book ? 
+        <>
+        <div className="spells">
+          {spells.map(spell => <div className="spell" key={spell.id}><SpellInfo spell={spell} spell_book_id={spell_book.id} spell_spell_books={this.props.spell_spell_books} /></div>)}
+        </div>
+      
+        <div>
+          <button onClick={handleClick} value={spell_book.id}>Delete Spell Book</button>
+        </div>
+        </>
+      : null}
+    </div>  
+  )   
 }
 
-export default SpellBookShow
+export default connect(null, {deleteSpellBook})(SpellBookShowFunc)
