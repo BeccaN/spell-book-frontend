9878f1f61aadda46427af4b21ed0c372fe6bc0ae
rncongi <rncongi@gmail.com>
2021-05-20 18:11:11 -0400

    Removed old files

--- src/_old/reducers/spellReducer.js
+++
@@ -1,9 +0,0 @@
-export default function spellReducer(state = {spells: []}, action) {
-  switch(action.type) {
-    case 'FETCH_SPELLS': {
-      return {spells: action.payload}
-    }
-    default:
-      return state
-  }
-}
